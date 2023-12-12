7093cc53d81132ef74e38689faf7fb687f76333a
91Toto31 <146136703+91Toto31@users.noreply.github.com>
2023-12-12 21:03:49 +0100

    Update lastOMP.c

--- lastOMP.c
+++ lastOMP.c
@@ -102,6 +102,9 @@ void initialCenters( double patterns[][Nv], double centers[][Nv] ) {
 
     int centerIndex ;
     size_t i, j ;
+	#pragma omp parallel shared(patterns, centers) private (centerIndex,i,j)
+{
+	#pragma omp for
     for ( i = 0; i < Nc; i++ ) {
         // split patterns in Nc blocks of N/Nc length
         // use rand and % to pick a random number of each block.
@@ -110,7 +113,7 @@ void initialCenters( double patterns[][Nv], double centers[][Nv] ) {
             centers[i][j] = patterns[centerIndex][j] ;
         }
     }
-
+} // fin para
     return ;
 }
 
